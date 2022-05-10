58c1860f88bd4cdcf953a831a70dfdc16fc781cb
7AngelMedina7 <101835095+7AngelMedina7@users.noreply.github.com>
2022-05-10 06:22:48 -0500

    Initial commit

---
+++ personas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Data(models.Model):
+    id= models.CharField(auto_created=True, primary_key=True, max_length=6)
+    nombres=models.CharField(max_length=20)
+    apellidos=models.CharField(max_length=20)
+    email=models.EmailField(max_length=30)
+    age=models.PositiveSmallIntegerField(max_length=2)
+
+
+    def __str__(self):
+        texto= "({0}) {1} {2}"
+        return texto.format(self.id, self.nombres, self.apellidos)
---
+++ personas/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Data
+
+# Create your views here.'\
+def home(request):
+    datos_personas = Data.objects.all()
+    # Se envia los datos
+    return render(request, "index.html", {"datos": datos_personas})
