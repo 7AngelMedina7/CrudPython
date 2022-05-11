59d1734ad816a146b679b2a8148099f98ce4bbd4
7AngelMedina7 <101835095+7AngelMedina7@users.noreply.github.com>
2022-05-10 23:27:28 -0500

    Revert "cambioos"
    This reverts commit 315c42686bcade8fed3acb073279b86f493f2765.

--- personas/urls.py
+++ personas/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('registrarDatos/', views.registrarDatos)
-    
+    path('', views.home)
 ]
--- registros/urls.py
+++ registros/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("personas.urls")),
     path('login', include("personas.urls"))
-    
 ]
