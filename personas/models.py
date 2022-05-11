485f4a08b2acd77c1a1689c6d8f00c3a70abd609
7AngelMedina7 <101835095+7AngelMedina7@users.noreply.github.com>
2022-05-11 00:18:24 -0500

    tesdt

--- personas/models.py
+++ personas/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Data(models.Model):
-    id= models.CharField(auto_created=True, primary_key=True, max_length=6)
-    nombres=models.CharField(max_length=20)
-    apellidos=models.CharField(max_length=20)
-    email=models.EmailField(max_length=30)
-    age=models.PositiveSmallIntegerField()
-    password= models.CharField(max_length=15)
+    id= models.AutoField(primary_key=True)
+    nombres=models.CharField(max_length=20, null=True)
+    apellidos=models.CharField(max_length=20, null=True)
+    email=models.EmailField(max_length=30, null=True)
+    age=models.PositiveSmallIntegerField(null=True)
+    password= models.CharField(max_length=15, null=True)
+
 
 
     def __str__(self):
--- personas/views.py
+++ personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Data
 
 # Create your views here.'\
@@ -6,3 +6,22 @@ def home(request):
     datos_personas = Data.objects.all()
     # Se envia los datos
     return render(request, "Login.html", {"datos": datos_personas})
+
+def registrarDatos(request):
+    nombres = request.POST['input_name']
+    apellidos = request.POST['input_lm']
+    email= request.POST ['input_email']
+    age= request.POST ['input_age']
+    password= request.POST ['input_pass']
+    id= request.POST ['input_id']
+
+    data = Data.objects.create(nombres=nombres, apellidos=apellidos, email=email, age=age, password=password, id=id)
+    print(data)
+    print("TEST!!!!!")
+    print("TEST!!!!!")
+    print("TEST!!!!!")
+    print("TEST!!!!!")
+    print("TEST!!!!!")
+    print("TEST!!!!!")
+    # mandando datos a la variable datos
+    return redirect('/')
