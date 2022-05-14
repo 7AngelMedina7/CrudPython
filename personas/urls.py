c781698ef31370d2c37dd3310a5a1edcd2830894
7AngelMedina7 <101835095+7AngelMedina7@users.noreply.github.com>
2022-05-13 22:03:55 -0500

    Final

--- personas/urls.py
+++ personas/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('registrarDatos/', views.registrarDatos)
+    path('registrarDatos/', views.registrarDatos),
+    path('edicionDatos/<id>', views.edicionDatos),
+    path('editarDatos/', views.editarDatos),
+    path('eliminarDatos/<id>', views.eliminarDatos)
 ]
--- personas/views.py
+++ personas/views.py
@@ -19,3 +19,32 @@ def registrarDatos(request):
     
     # mandando datos a la variable datos en /
     return redirect('/')
+
+def edicionDatos(request,id):
+    data = Data.objects.get(id=id)
+    return render(request, "edicionDatos.html",{"data":data})
+
+def editarDatos(request):
+    id= request.POST ['id_']
+    nombres = request.POST['name_']
+    apellidos = request.POST['apellido_']
+    email= request.POST ['email_']
+    age= request.POST ['edad_']
+    password= request.POST ['contra_']
+
+    data =  Data.objects.get(id=id)
+    data.nombres = nombres
+    data.apellidos = apellidos
+    data.email = email
+    data.age = age
+    data.password = password
+    data.save()
+
+    return redirect('/')
+
+def eliminarDatos(request, id):
+    data = Data.objects.get(id=id)
+    data.delete()
+
+    #mandar ruta inicial o ruta raiz
+    return redirect('/')
